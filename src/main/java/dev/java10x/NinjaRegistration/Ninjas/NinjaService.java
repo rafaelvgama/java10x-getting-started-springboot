6c20a17bf3c1a3616f5535983db5d6d5ed6c8f8d
Rafael Vogas Gama <rafaelvgama@yahoo.com.br>
2025-09-29 03:25:53 -0300

    feat: Adding DTO as abstraction for NinjaModel in service and controller layers

--- src/main/java/dev/java10x/NinjaRegistration/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/NinjaRegistration/Ninjas/NinjaService.java
@@ -4,15 +4,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class NinjaService {
 
     private final NinjaRepository ninjaRepository;
+    private final NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     public List<NinjaModel> listNinjas() {
@@ -24,8 +25,10 @@ public class NinjaService {
         return ninja.orElse(null);
     }
 
-    public NinjaModel createNinja(NinjaModel ninja) {
-        return ninjaRepository.save(ninja);
+    public NinjaDTO createNinja(NinjaDTO ninjaDTO) {
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     public NinjaModel updateNinja(Long id, NinjaModel ninja) {
